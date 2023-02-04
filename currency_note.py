3069e4147d942ebaa994b0155307d2b51f39e512
Logesh Kumar <120043064+logeshk11@users.noreply.github.com>
2023-02-05 00:03:12 +0530

    Add files via upload

---
+++ currency_note.py
@@ -0,0 +1,20 @@
+# calculates the least number of coins and rupee note to give for any value in INR
+import math
+
+notes = [1, 2, 5, 10, 20, 50, 100, 200, 2000, 500]
+
+
+def minimum_notes(money):
+    no_of_note = 0
+    for a in range(len(notes) - 1, 0, -1):
+        cal = money / notes[a]
+        if cal >= 1:
+            no_of_note = math.floor(cal)
+            money -= (no_of_note * notes[a])
+            print(f"You need {no_of_note} note in {notes[a]}")
+
+
+money=int(input("Enter the value:"))
+
+notes.sort()
+(minimum_notes(money))
