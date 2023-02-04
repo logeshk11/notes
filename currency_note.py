f6509ffdc9abc1bbbc04f145f46276a2ff00d36b
Logesh Kumar <120043064+logeshk11@users.noreply.github.com>
2023-02-05 00:04:01 +0530

    Update currency_note.py

--- currency_note.py
+++ currency_note.py
@@ -5,7 +5,6 @@ notes = [1, 2, 5, 10, 20, 50, 100, 200, 2000, 500]
 
 
 def minimum_notes(money):
-    no_of_note = 0
     for a in range(len(notes) - 1, 0, -1):
         cal = money / notes[a]
         if cal >= 1:
